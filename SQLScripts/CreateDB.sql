689f00e5266d2727b66f6d6b984d946d5b8cd330
Mikhail Smirnoff <mike-smirnoff@yandex.ru>
2015-07-19 22:15:16 +0400

    Добавлены скрипты создания таблиц SymbolTypes and Symbols

--- SQLScripts/CreateDB.sql
+++ SQLScripts/CreateDB.sql
@@ -9,3 +9,19 @@ CREATE TABLE InvestmentPortfolio.dbo.Portfolios (
   CONSTRAINT PK_Portfolios PRIMARY KEY NONCLUSTERED (id)
 ) ON [PRIMARY]
 GO
+
+CREATE TABLE InvestmentPortfolio.dbo.SymbolTypes (
+  stype VARCHAR(50) NOT NULL,
+  name VARCHAR(100) NULL,
+  CONSTRAINT PK_SymbolTypes PRIMARY KEY CLUSTERED (stype)
+) ON [PRIMARY]
+GO
+
+CREATE TABLE InvestmentPortfolio.dbo.Symbols (
+  ticker VARCHAR(50) NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  stype VARCHAR(50) NULL,
+  CONSTRAINT PK_Symbols PRIMARY KEY CLUSTERED (ticker),
+  FOREIGN KEY (stype) REFERENCES dbo.SymbolTypes (stype)
+) ON [PRIMARY]
+GO
